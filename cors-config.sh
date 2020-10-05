a53bf2b72155655cb6250532c00ea3c8461708b8
CryptoNightCANADA <49769595+CryptonightCanada@users.noreply.github.com>
2020-10-05 14:54:06 -0400

    Update cors-config.sh

--- cors-config.sh
+++ cors-config.sh
@@ -10,3 +10,4 @@ ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]
 
 echo "IPFS API CORS headers configured for $ALLOW_ORIGINS"
 echo "Please restart your IPFS daemon"
+
